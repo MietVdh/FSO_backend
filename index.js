004bf0b576212e7f4adf99e37668a1273368a7b5
MietVdh <miet.vanderheyden@gmail.com>
2025-05-06 11:54:44 -0400

    Added database

--- index.js
+++ index.js
@@ -1,54 +1,28 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
+
 const app = express()
 
-const cors = require('cors')
-
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-    return String(maxId + 1)
-}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
 
 app.get('/api/notes/:id', (request, response) => {
-const id = request.params.id
-const note = notes.find(note => note.id === id)
+Note.findById(request.params.id).then(note => {
+  response.json(note)
+})
 
-if (note) {
-    response.json(note)
-} else {
-    response.status(404).end()
-}
 })
   
 app.get('/api/notes', (request, response) => {
-response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+  
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -66,18 +40,18 @@ if (!body.content) {
     })
 }
 
-const note = {
+const note = new Note( {
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-}
-
-notes = notes.concat(note)
-
-response.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+note.save().then(savedNote => {
+  response.json(savedNote)
+})
+
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
